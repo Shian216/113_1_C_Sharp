98c7994355a3847de22bc959bd44528ba5f104b6
Shian216 <a113221019@mail.shu.edu.tw>
2024-10-09 09:17:45 +0800

    113-10-9

---
+++ HW1/HW1/HW1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = "黑桃10";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = "黑桃J";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = "黑桃Q";
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = "黑桃K";
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            lblShow.Text = "黑桃A";
+        }
+    }
+}
