aa44c685a610dd366245f62a18add544ee94e27b
Shian216 <a113221019@mail.shu.edu.tw>
2024-10-15 17:57:35 +0800

    113-10-15

---
+++ 113-10-15/Tutorial3_2/Tutorial3_2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial3_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double kms;
+            double liters;
+            //double average;
+
+            kms = double.Parse(txbKM.Text);
+            liters = double.Parse(txbLiter.Text);
+            //average = (kms / liters).ToString();
+
+            lblShow.Text = (kms / liters).ToString("f3");
+            //lblShow.Text = average.ToString("n3");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
