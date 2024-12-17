efcbf107779178fc15ec185bb21e109399a1d687
Shian216 <a113221019@mail.shu.edu.tw>
2024-12-17 17:52:42 +0800

    113-12-17

--- 113-12-10/Tutorial 6-1/Lights/Lights/Form1.cs
+++ 113-12-10/Tutorial 6-1/Lights/Lights/Form1.cs
@@ -12,20 +12,45 @@ namespace Lights
 {
     public partial class Form1 : Form
     {
+        // 用來追蹤燈光開關狀態的變數（開/關）
+        private bool isLightOn = false;
 
         public Form1()
         {
             InitializeComponent();
         }
+
         private void switchButton_Click(object sender, EventArgs e)
         {
-           
+            // 切換燈光的開關狀態
+            isLightOn = !isLightOn;
+
+            // 根據燈光狀態來顯示不同的圖片並更新標籤文字
+            if (isLightOn)
+            {
+                // 顯示 lightOnPictureBox，隱藏 lightOffPictureBox
+                lightOnPictureBox.Visible = true;
+                lightOffPictureBox.Visible = false;
+
+                // 更新標籤文字為 "ON"
+                lightStateLabel.Text = "ON";
+            }
+            else
+            {
+                // 顯示 lightOffPictureBox，隱藏 lightOnPictureBox
+                lightOnPictureBox.Visible = false;
+                lightOffPictureBox.Visible = true;
+
+                // 更新標籤文字為 "OFF"
+                lightStateLabel.Text = "OFF";
+            }
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-            // Close the form.
+            // 關閉窗體
             this.Close();
         }
     }
 }
+
